32ea9cd3274d7b87120f783255d45010751afc13
Harrmit Chauhan <46776916+harmit17@users.noreply.github.com>
2023-02-03 16:51:46 -0500

    added python project

---
+++ Calculator.oy.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  3 16:48:03 2023
+
+@author: harmi
+"""
+
+def add(a, b):
+    pass
+def subtract(a, b):
+    pass
+def multiply(a, b):
+    pass
+def divide(a, b):
+    pass
+
+
